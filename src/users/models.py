005ea9452611f7cfafe83252ee01226ad09537a3
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2022-01-21 00:06:37 +0100

    deleted the file models.py

--- src/users/models.py
+++
@@ -1,8 +0,0 @@
-# from django.db import models
-# from django.contrib.auth.models import User
-
-
-# class Profile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     bio = models.CharField(max_length=200, blank=True)
-#     location = models.CharField(max_length=100, blank=True)
