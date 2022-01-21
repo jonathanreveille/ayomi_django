2c8a78578fc3ce70894576beefeab7034a4eaa2d
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2022-01-21 11:48:46 +0100

    Changed language to english for settings. Updated button from register to login

--- src/core/settings.py
+++ src/core/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'fr-FR'
+LANGUAGE_CODE = 'en-US'
 
 TIME_ZONE = 'Europe/Paris'
 
